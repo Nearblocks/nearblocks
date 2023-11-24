34a879f222677f49c2c6e92f037a9e2461cad8a4
Jamal <jamal@invoker.lol>
2023-11-24 10:24:53 +0530

    fix: temporarily use testnet components

--- apps/bos-components/.eslintrc.cjs
+++ apps/bos-components/.eslintrc.cjs
@@ -13,5 +13,7 @@ module.exports = {
     ],
     '@typescript-eslint/ban-ts-comment': 'off',
     'react/display-name': 'off',
+    '@next/next/no-img-element': 'off',
+    'import/no-anonymous-default-export': 'off',
   },
 };
