6015d6aea0d32f270ec09def9709c2c501cbe441
Ch3rry B@ry <cherrybary21@gmail.com>
2023-11-28 10:20:26 +0530

    add ssl support to to migrations

--- apps/backend/knexfile.js
+++ apps/backend/knexfile.js
@@ -1,7 +1,20 @@
+const ssl = {
+  rejectUnauthorized: true,
+};
+
+if (process.env.DATABASE_CA) {
+  ssl.ca = Buffer.from(process.env.DATABASE_CA, 'base64').toString('utf-8');
+  ssl.cert = Buffer.from(process.env.DATABASE_CERT, 'base64').toString('utf-8');
+  ssl.key = Buffer.from(process.env.DATABASE_KEY, 'base64').toString('utf-8');
+}
+
 export default {
   production: {
     client: 'postgresql',
-    connection: process.env.DATABASE_URL,
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: ssl?.ca ? ssl : false,
+    },
     migrations: {
       directory: './migrations',
       tableName: 'knex_migrations',
