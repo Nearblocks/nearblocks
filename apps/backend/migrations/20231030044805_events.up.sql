3ded93d29b9b61d0f44e8e899432e84d4785b5fd
Ch3rry B@ry <cherrybary21@gmail.com>
2023-12-30 10:38:52 +0530

    chore: format yml and sql files

--- apps/backend/migrations/20231030044805_events.up.sql
+++ apps/backend/migrations/20231030044805_events.up.sql
@@ -1,10 +1,4 @@
-CREATE TYPE event_cause AS ENUM (
-  'MINT',
-  'TRANSFER',
-  'BURN'
-);
-
-
+CREATE TYPE event_cause AS ENUM('MINT', 'TRANSFER', 'BURN');
 
 CREATE TABLE ft_events (
   event_index NUMERIC(38, 0) NOT NULL,
@@ -17,13 +11,10 @@ CREATE TABLE ft_events (
   cause event_cause,
   status event_status,
   event_memo TEXT,
-	delta_amount NUMERIC(40) NOT NULL,
-	absolute_amount NUMERIC(40),
+  delta_amount NUMERIC(40) NOT NULL,
+  absolute_amount NUMERIC(40),
   block_timestamp BIGINT NOT NULL,
-  PRIMARY KEY (
-    event_index,
-    block_timestamp
-  )
+  PRIMARY KEY (event_index, block_timestamp)
 );
 
 CREATE TABLE nft_events (
@@ -41,32 +32,33 @@ CREATE TABLE nft_events (
   event_memo TEXT,
   delta_amount SMALLINT NOT NULL,
   block_timestamp BIGINT NOT NULL,
-  PRIMARY KEY (
-    event_index,
-    block_timestamp
-  )
+  PRIMARY KEY (event_index, block_timestamp)
 );
-
-
 
 CREATE INDEX fte_contract_account_id_idx ON ft_events USING btree (contract_account_id);
 
 CREATE INDEX fte_affected_account_id_idx ON ft_events USING btree (affected_account_id);
 
-
-
 CREATE INDEX nfte_contract_account_id_idx ON nft_events USING btree (contract_account_id);
 
 CREATE INDEX nfte_affected_account_id_idx ON nft_events USING btree (affected_account_id);
 
+SELECT
+  create_hypertable (
+    'ft_events',
+    'block_timestamp',
+    chunk_time_interval = > 604800000000000
+  );
 
+SELECT
+  create_hypertable (
+    'nft_events',
+    'block_timestamp',
+    chunk_time_interval = > 3628800000000000
+  );
 
-SELECT create_hypertable ('ft_events', 'block_timestamp', chunk_time_interval => 604800000000000);
+SELECT
+  set_integer_now_func ('ft_events', 'epoch_nano_seconds');
 
-SELECT create_hypertable ('nft_events', 'block_timestamp', chunk_time_interval => 3628800000000000);
-
-
-
-SELECT set_integer_now_func('ft_events', 'epoch_nano_seconds');
-
-SELECT set_integer_now_func('nft_events', 'epoch_nano_seconds');
+SELECT
+  set_integer_now_func ('nft_events', 'epoch_nano_seconds');
