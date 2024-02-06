0c9d0f9d26aa666f6d72ce862955fa8b9ed99705
Ch3rry B@ry <cherrybary21@gmail.com>
2024-02-06 17:24:07 +0530

    fix: update errored contract unique indexes

---
+++ apps/backend/migrations/20240206090209_errored_contracts_index.up.sql
@@ -0,0 +1,10 @@
+DROP CONSTRAINT "errored_contracts_contract_type_key",
+DROP CONSTRAINT "errored_contracts_contract_type_token_key";
+
+CREATE UNIQUE INDEX ec_unique_contract_type ON errored_contracts (contract, type)
+WHERE
+  (token IS NULL);
+
+CREATE UNIQUE INDEX ec_unique_contract_type_token ON errored_contracts (contract, type, token)
+WHERE
+  (token IS NOT NULL);
