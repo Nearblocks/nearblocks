ddc2d68f852e020dfc72e3725b87faad161767ff
Cherry Bary <84170175+cherrybarry@users.noreply.github.com>
2024-02-12 14:10:18 +0530

    chore(explorer-selector): update legacy url rewrites (#146)

--- apps/explorer-selector/next.config.js
+++ apps/explorer-selector/next.config.js
@@ -17,11 +17,32 @@ const nextConfig = {
   async rewrites() {
     return [
       {
+        source: '/api/nodes',
+        destination:
+          process.env.NEXT_PUBLIC_NETWORK_ID === 'mainnet'
+            ? 'https://api3.nearblocks.io/v1/node/telemetry'
+            : 'https://api3-testnet.nearblocks.io/v1/node/telemetry',
+      },
+      {
+        source: '/api/circulating-supply',
+        destination:
+          process.env.NEXT_PUBLIC_NETWORK_ID === 'mainnet'
+            ? 'https://api3.nearblocks.io/v1/legacy/circulating-supply'
+            : 'https://api3-testnet.nearblocks.io/v1/legacy/circulating-supply',
+      },
+      {
+        source: '/api/circulating-supply-in-near',
+        destination:
+          process.env.NEXT_PUBLIC_NETWORK_ID === 'mainnet'
+            ? 'https://api3.nearblocks.io/v1/legacy/circulating-supply?unit=near'
+            : 'https://api3-testnet.nearblocks.io/v1/legacy/circulating-supply?unit=near',
+      },
+      {
         source: '/api/:path*',
         destination:
           process.env.NEXT_PUBLIC_NETWORK_ID === 'mainnet'
-            ? 'https://api3.nearblocks.io/v1/legacy/:path*'
-            : 'https://api3-testnet.nearblocks.io/v1/legacy/:path*',
+            ? 'https://legacy.explorer.near.org/api/:path*'
+            : 'https://legacy.explorer.testnet.near.org/api/:path*',
       },
     ];
   },
