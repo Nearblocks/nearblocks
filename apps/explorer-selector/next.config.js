e2d4844235da60b2d9688923227c29be1d3be140
Ch3rry B@ry <cherrybary21@gmail.com>
2024-02-12 15:19:55 +0530

    update fees url

--- apps/explorer-selector/next.config.js
+++ apps/explorer-selector/next.config.js
@@ -38,6 +38,20 @@ const nextConfig = {
             : 'https://api3-testnet.nearblocks.io/v1/legacy/circulating-supply?unit=near',
       },
       {
+        source: '/api/fees-of-previous-day-utc',
+        destination:
+          process.env.NEXT_PUBLIC_NETWORK_ID === 'mainnet'
+            ? 'https://api3.nearblocks.io/v1/legacy/fees?pediod=day'
+            : 'https://api3-testnet.nearblocks.io/v1/legacy/fees?pediod=day',
+      },
+      {
+        source: '/api/fees-of-previous-7-days-utc',
+        destination:
+          process.env.NEXT_PUBLIC_NETWORK_ID === 'mainnet'
+            ? 'https://api3.nearblocks.io/v1/legacy/fees?pediod=week'
+            : 'https://api3-testnet.nearblocks.io/v1/legacy/fees?pediod=week',
+      },
+      {
         source: '/api/:path*',
         destination:
           process.env.NEXT_PUBLIC_NETWORK_ID === 'mainnet'
