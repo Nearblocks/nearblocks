a2e4a0a0474edaf580adbd79a1122445c7bf6a8b
Ch3rry B@ry <cherrybary21@gmail.com>
2024-02-19 17:37:41 +0530

    feat(api): add kitwallet apis

--- apps/api/src/libs/schema/kitwallet.ts
+++ apps/api/src/libs/schema/kitwallet.ts
@@ -1,5 +1,9 @@
 import { z } from 'zod';
 
+const accounts = z.object({
+  key: z.string(),
+});
+
 const tokens = z.object({
   account: z.string(),
 });
@@ -9,10 +13,25 @@ const tokensFromBlock = z.object({
   fromBlockTimestamp: z.number().optional().default(0),
 });
 
+const nfts = z.object({
+  account: z.string(),
+});
+
+const nftsFromBlock = z.object({
+  account: z.string(),
+  fromBlockTimestamp: z.number().optional().default(0),
+});
+
+export type Accounts = z.infer<typeof accounts>;
 export type Tokens = z.infer<typeof tokens>;
 export type TokensFromBlock = z.infer<typeof tokensFromBlock>;
+export type Nfts = z.infer<typeof nfts>;
+export type NftsFromBlock = z.infer<typeof nftsFromBlock>;
 
 export default {
+  accounts,
+  nfts,
+  nftsFromBlock,
   tokens,
   tokensFromBlock,
 };
--- apps/api/src/routes/kitwallet.ts
+++ apps/api/src/routes/kitwallet.ts
@@ -12,6 +12,23 @@ const routes = (app: Router) => {
   app.use('/kitwallet', bearerAuth, rateLimiter, route);
 
   /**
+   * GET /v1/kitwallet/staking-pools
+   * @summary Get all staking pools
+   * @tags Kitwallet
+   * @return 200 - success response
+   */
+  route.get('/staking-pools', kitwallet.pools);
+
+  /**
+   * GET /v1/kitwallet/{key}/accounts
+   * @summary Get accounts by public key
+   * @tags Kitwallet
+   * @param {string} key.path.required - public key
+   * @return 200 - success response
+   */
+  route.get('/:key/accounts', validator(schema.accounts), kitwallet.accounts);
+
+  /**
    * GET /v1/kitwallet/{account}/likely-tokens
    * @summary Get likely tokens for an account
    * @tags Kitwallet
@@ -36,6 +53,29 @@ const routes = (app: Router) => {
     '/:account/likely-tokens-from-block',
     validator(schema.tokensFromBlock),
     kitwallet.tokensFromBlock,
+  );
+
+  /**
+   * GET /v1/kitwallet/{account}/likely-nfts
+   * @summary Get likely nfts for an account
+   * @tags Kitwallet
+   * @param {string} account.path.required - account id
+   * @return 200 - success response
+   */
+  route.get('/:account/likely-nfts', validator(schema.nfts), kitwallet.nfts);
+
+  /**
+   * GET /v1/kitwallet/{account}/likely-nfts-from-block
+   * @summary Get likely nfts for an account from block
+   * @tags Kitwallet
+   * @param {string} account.path.required - account id
+   * @param {string} fromBlockTimestamp.query - block timestamp
+   * @return 200 - success response
+   */
+  route.get(
+    '/:account/likely-nfts-from-block',
+    validator(schema.nftsFromBlock),
+    kitwallet.nftsFromBlock,
   );
 };
 
