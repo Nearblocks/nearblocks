71c817258e2e3d3cabcf185eb5457eb31ec5b6bd
Cherry Bary <84170175+cherrybarry@users.noreply.github.com>
2024-02-19 17:42:23 +0530

    feat(api): add kitwallet apis (#161)

--- apps/api/src/services/kitwallet.ts
+++ apps/api/src/services/kitwallet.ts
@@ -1,11 +1,28 @@
 import { Response } from 'express';
 
+import { Network } from 'nb-types';
+
+import config from '#config';
 import catchAsync from '#libs/async';
 import sql from '#libs/postgres';
-import { Tokens, TokensFromBlock } from '#libs/schema/kitwallet';
+import {
+  Accounts,
+  Nfts,
+  NftsFromBlock,
+  Tokens,
+  TokensFromBlock,
+} from '#libs/schema/kitwallet';
 import { RequestValidator } from '#types/types';
 
 const BRIDGE_TOKEN_FACTORY = 'factory.bridge.near';
+const POOLS =
+  config.network === Network.MAINNET
+    ? ['%.poolv1.near', '%.pool.near']
+    : [
+        '%.pool.%.m0',
+        '%.factory01.littlefarm.testnet',
+        '%.factory.colorpalette.testnet',
+      ];
 
 const getLastBlockTimestamp = async () => {
   const [{ block_timestamp: lastTimestamp }] = await sql`
@@ -77,6 +94,37 @@ const getLikelyTokens = async (
   return tokens;
 };
 
+const getLikelyNfts = async (
+  account: string,
+  timestamp: number,
+  lastTimestamp: string,
+) => {
+  const tokens = await sql`
+    SELECT DISTINCT
+      receipt_receiver_account_id AS receiver_account_id
+    FROM
+      action_receipt_actions
+    WHERE
+      args -> 'args_json' ->> 'receiver_id' = ${account}
+      AND action_kind = 'FUNCTION_CALL'
+      AND args ->> 'args_json' IS NOT NULL
+      AND args ->> 'method_name' LIKE 'nft_%'
+      AND receipt_included_in_block_timestamp <= ${lastTimestamp}
+      AND receipt_included_in_block_timestamp > ${timestamp}
+    UNION
+    SELECT DISTINCT
+      contract_account_id AS receiver_account_id
+    FROM
+      nft_events
+    WHERE
+      affected_account_id = ${account}
+      AND block_timestamp <= ${lastTimestamp}
+      AND block_timestamp > ${timestamp}
+  `;
+
+  return tokens;
+};
+
 const tokens = catchAsync(
   async (req: RequestValidator<Tokens>, res: Response) => {
     const account = req.validator.data.account;
@@ -110,4 +158,71 @@ const tokensFromBlock = catchAsync(
   },
 );
 
-export default { tokens, tokensFromBlock };
+const nfts = catchAsync(async (req: RequestValidator<Nfts>, res: Response) => {
+  const account = req.validator.data.account;
+
+  const lastBlockTimestamp = await getLastBlockTimestamp();
+  const nfts = await getLikelyNfts(account, 0, lastBlockTimestamp);
+
+  return res
+    .status(200)
+    .json(nfts.map(({ receiver_account_id }) => receiver_account_id));
+});
+
+const nftsFromBlock = catchAsync(
+  async (req: RequestValidator<NftsFromBlock>, res: Response) => {
+    const account = req.validator.data.account;
+    const timestamp = req.validator.data.fromBlockTimestamp;
+
+    const lastBlockTimestamp = await getLastBlockTimestamp();
+    const nfts = await getLikelyNfts(account, timestamp, lastBlockTimestamp);
+
+    return res.status(200).json({
+      lastBlockTimestamp,
+      list: nfts.map(({ receiver_account_id }) => receiver_account_id),
+      version: '1.0.0',
+    });
+  },
+);
+
+const accounts = catchAsync(
+  async (req: RequestValidator<Accounts>, res: Response) => {
+    const key = req.validator.data.key;
+
+    const accounts = await sql`
+      SELECT DISTINCT
+        account_id
+      FROM
+        access_keys
+        JOIN accounts USING (account_id)
+      WHERE
+        public_key = ${key}
+        AND accounts.deleted_by_receipt_id IS NULL
+        AND access_keys.deleted_by_receipt_id IS NULL
+    `;
+
+    return res.status(200).json(accounts.map(({ account_id }) => account_id));
+  },
+);
+
+const pools = catchAsync(async (_req: Request, res: Response) => {
+  const pools = await sql`
+    SELECT
+      account_id
+    FROM
+      accounts
+    WHERE
+      account_id LIKE ANY (${POOLS})
+  `;
+
+  return res.status(200).json(pools.map(({ account_id }) => account_id));
+});
+
+export default {
+  accounts,
+  nfts,
+  nftsFromBlock,
+  pools,
+  tokens,
+  tokensFromBlock,
+};
