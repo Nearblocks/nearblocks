c2d1a9c374579b8cea42389d0f197598d386fa3a
Andrew Lau <andrew@andrewklau.com>
2024-03-19 07:04:08 +0700

    fix(backend): remove concurrent index migration

--- apps/backend/migrations/20240315131046_event_indexes.up.sql
+++ apps/backend/migrations/20240315131046_event_indexes.up.sql
@@ -1,11 +1,11 @@
-CREATE INDEX CONCURRENTLY IF NOT EXISTS t_fte_contract_account_id_event_index_idx ON ft_events (contract_account_id, event_index DESC);
+CREATE INDEX IF NOT EXISTS t_fte_contract_account_id_event_index_idx ON ft_events (contract_account_id, event_index DESC);
 
-CREATE INDEX CONCURRENTLY IF NOT EXISTS t_fte_affected_account_id_event_index_idx ON ft_events (affected_account_id, event_index DESC);
+CREATE INDEX IF NOT EXISTS t_fte_affected_account_id_event_index_idx ON ft_events (affected_account_id, event_index DESC);
 
-CREATE INDEX CONCURRENTLY IF NOT EXISTS t_fte_block_height_idx ON ft_events (block_height);
+CREATE INDEX IF NOT EXISTS t_fte_block_height_idx ON ft_events (block_height);
 
-CREATE INDEX CONCURRENTLY IF NOT EXISTS t_nfte_contract_account_id_event_index_idx ON nft_events (contract_account_id, event_index DESC);
+CREATE INDEX IF NOT EXISTS t_nfte_contract_account_id_event_index_idx ON nft_events (contract_account_id, event_index DESC);
 
-CREATE INDEX CONCURRENTLY IF NOT EXISTS t_nfte_affected_account_id_event_index_idx ON nft_events (affected_account_id, event_index DESC);
+CREATE INDEX IF NOT EXISTS t_nfte_affected_account_id_event_index_idx ON nft_events (affected_account_id, event_index DESC);
 
-CREATE INDEX CONCURRENTLY IF NOT EXISTS t_nfte_block_height_idx ON nft_events (block_height);
+CREATE INDEX IF NOT EXISTS t_nfte_block_height_idx ON nft_events (block_height);
