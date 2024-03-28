057cae00f24af1ef55edfa09c34845a2492c266e
yudho <yadudevpro@gmail.com>
2024-03-28 22:33:36 +0530

    fix(bos-components): es-lint error

--- apps/app/src/pages/hash/[receipt].tsx
+++ apps/app/src/pages/hash/[receipt].tsx
@@ -24,7 +24,10 @@ export const getServerSideProps = async (context: any) => {
           ? `https://api3.nearblocks.io/v1/search/receipts?keyword=${receipt}`
           : `https://api3-testnet.nearblocks.io/v1/search/receipts?keyword=${receipt}`,
         {
-          headers: { Authorization: `Bearer ${process.env.API_KEY}` },
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
         },
       );
 
