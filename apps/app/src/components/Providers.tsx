3fbc6cace58da021750ce29712463bd2b721320c
yudho <yadudevpro@gmail.com>
2024-04-19 13:31:03 +0530

    fix(app): resolve flash issue with dark mode styles

---
+++ apps/app/src/components/Providers.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import { ThemeProvider } from 'next-themes';
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <ThemeProvider attribute="class" enableSystem={false}>
+      {' '}
+      {children}{' '}
+    </ThemeProvider>
+  );
+}
