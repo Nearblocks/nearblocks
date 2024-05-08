302c2aa1179fb2ac32dac90c7c76fc0baf16d7dd
yudho <yadudevpro@gmail.com>
2024-05-08 17:39:35 +0530

    fix(bos-components): error message display issue and update translation in ft and nft

--- apps/bos-components/src/components/NFT/TransfersList.tsx
+++ apps/bos-components/src/components/NFT/TransfersList.tsx
@@ -359,7 +359,7 @@ export default function ({ network, t, ownerId }: Props) {
       cell: (row: TransactionInfo) => {
         return row.affected_account_id === row.involved_account_id ? (
           <span className="uppercase rounded w-10 py-2 h-6 inline-flex items-center justify-center bg-green-200 text-white text-sm font-semibold">
-            {t('txnSelf')}
+            {t('txns:txnSelf')}
           </span>
         ) : (
           <div className="w-5 h-5 p-1 bg-green-100 rounded-full text-center flex justify-center items-center mx-auto text-white">
