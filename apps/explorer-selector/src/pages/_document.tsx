d883d0905d34496c356b29de6bea4b64527080ba
yudho <yadudevpro@gmail.com>
2024-07-22 18:11:15 +0530

    fix(bos+selector): remove legacy explorer and add lite explorer

--- apps/explorer-selector/src/pages/_document.tsx
+++ apps/explorer-selector/src/pages/_document.tsx
@@ -9,11 +9,6 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600&display=swap"
           rel="stylesheet"
         />
-        <link
-          rel="stylesheet"
-          type="text/css"
-          href="https://beta.nearblocks.io/common.css"
-        />
         <link rel="icon" href="/favicon.ico" />
         <link rel="manifest" href="/site.webmanifest" />
         {/* eslint-disable-next-line @next/next/no-sync-scripts */}
