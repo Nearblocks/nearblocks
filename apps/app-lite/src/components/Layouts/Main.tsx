2b4fd7c0caead6d48cebd96c5911c28c6dda625d
iqbal <152838941+iqbaldevpro@users.noreply.github.com>
2024-07-25 10:04:34 +0530

    feat(app-lite): add custom rpc (#387)

--- apps/app-lite/src/components/Layouts/Main.tsx
+++ apps/app-lite/src/components/Layouts/Main.tsx
@@ -1,20 +1,35 @@
-import { ReactElement, ReactNode } from 'react';
+import { ReactElement, ReactNode, useEffect } from 'react';
 
 import Navbar from '@/components/Header/Navbar';
+import { useRpcStore } from '@/stores/rpc';
 
 interface LayoutProps {
   children: ReactNode;
   hideSearch?: boolean;
 }
 
-const Layout = ({ children, hideSearch }: LayoutProps) => (
-  <>
-    <header className="lg:pt-9">
-      <Navbar hideSearch={hideSearch} />
-    </header>
-    <main className="font-sans font-light text-text-body pb-9">{children}</main>
-  </>
-);
+const Layout = ({ children, hideSearch }: LayoutProps) => {
+  const setRpc = useRpcStore((state) => state.setRpc);
+
+  useEffect(() => {
+    const searchParams = new URLSearchParams(window.location.search);
+    const rpcUrl = searchParams.get('rpcUrl');
+    if (rpcUrl) {
+      setRpc(rpcUrl);
+    }
+  }, [setRpc]);
+
+  return (
+    <>
+      <header className="lg:pt-9">
+        <Navbar hideSearch={hideSearch} />
+      </header>
+      <main className="font-sans font-light text-text-body pb-9">
+        {children}
+      </main>
+    </>
+  );
+};
 
 const MainLayout: React.FC<{
   children: ReactElement;
