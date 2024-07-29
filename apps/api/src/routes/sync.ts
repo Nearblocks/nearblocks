bfef5e960651939a8f27bd2bf5ca4df6358d534b
Cherry Bary <84170175+cherrybarry@users.noreply.github.com>
2024-07-29 18:50:39 +0530

    feat(api): add individual status end points (#391)

--- apps/api/src/routes/sync.ts
+++ apps/api/src/routes/sync.ts
@@ -10,6 +10,12 @@ const routes = (app: Router) => {
   app.use('/sync', bearerAuth, rateLimiter, route);
 
   route.get('/status', sync.status);
+  route.get('/status/ft-holders', sync.ft);
+  route.get('/status/nft-holders', sync.nft);
+  route.get('/status/indexer-balance', sync.balance);
+  route.get('/status/indexer-base', sync.base);
+  route.get('/status/indexer-events', sync.events);
+  route.get('/status/daily-stats', sync.stats);
 };
 
 export default routes;
