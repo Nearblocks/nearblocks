64a8482a08ed5569e105de85719007d36e9c51cb
iqbal <152838941+iqbaldevpro@users.noreply.github.com>
2024-08-29 12:55:13 +0530

    feat(app-lite): add footer in lite explorer (#446)

--- apps/app-lite/src/components/Layouts/Main.tsx
+++ apps/app-lite/src/components/Layouts/Main.tsx
@@ -3,6 +3,8 @@ import { ReactElement, ReactNode, useEffect } from 'react';
 import Navbar from '@/components/Header/Navbar';
 import { useRpcStore } from '@/stores/rpc';
 
+import Footer from '../Footer';
+
 interface LayoutProps {
   children: ReactNode;
   hideSearch?: boolean;
@@ -20,14 +22,17 @@ const Layout = ({ children, hideSearch }: LayoutProps) => {
   }, [setRpc]);
 
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <header className="lg:pt-9">
         <Navbar hideSearch={hideSearch} />
       </header>
       <main className="font-sans font-light text-text-body pb-9">
         {children}
       </main>
-    </>
+      <footer className="pt-9 mt-auto">
+        <Footer />
+      </footer>
+    </div>
   );
 };
 
