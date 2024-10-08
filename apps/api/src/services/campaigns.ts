3c3e4948735dfbedee5f2b4e273a5a17bf31f89c
alexandra <reese2k24@gmail.com>
2024-10-08 15:06:29 +0530

    fix(api): update query

--- apps/api/src/services/campaigns.ts
+++ apps/api/src/services/campaigns.ts
@@ -4,7 +4,6 @@ import { Network } from 'nb-types';
 
 import config from '#config';
 import catchAsync from '#libs/async';
-import logger from '#libs/logger';
 import { userSql } from '#libs/postgres';
 import { Campaign } from '#types/types';
 
@@ -12,87 +11,86 @@ const baseUrl =
   config.network === Network.MAINNET ? config.mainnetUrl : config.testnetUrl;
 
 const getApprovedAds = catchAsync(async (req: Request, res: Response) => {
-  try {
-    const { type } = req.query;
-    const query = userSql<Campaign[]>`
-      WITH
-        valid_ads AS (
-          SELECT
-            *
-          FROM
-            campaign__ads
-          WHERE
-            is_approved = TRUE
-            AND is_active = TRUE
-            AND api_subscription_id IN (
-              SELECT
-                id
-              FROM
-                api__subscriptions
-              WHERE
-                status = 'active'
-            )
-            AND text IS NULL
-        ),
-        rand_selection AS (
-          SELECT
-            CEIL(
-              RANDOM() * (
-                SELECT
-                  MAX(id)
-                FROM
-                  valid_ads
-              )
-            ) AS rand_id
-        )
-      SELECT
-        *
-      FROM
-        valid_ads
-      WHERE
-        id >= (
-          SELECT
-            rand_id
-          FROM
-            rand_selection
-        )
-        OR id <= (
-          SELECT
-            rand_id
-          FROM
-            rand_selection
-        )
-      ORDER BY
-        CASE
-          WHEN id >= (
+  const { type } = req.query;
+  const query = userSql<Campaign[]>`
+    WITH
+      valid_ads AS (
+        SELECT
+          *
+        FROM
+          campaign__ads
+        WHERE
+          is_approved = TRUE
+          AND is_active = TRUE
+          AND api_subscription_id IN (
             SELECT
-              rand_id
+              id
             FROM
-              rand_selection
-          ) THEN 1
-          ELSE 2
-        END,
-        id
-      LIMIT
-        1;
-    `;
-    const result = await query;
-    const ad = result[0];
+              api__subscriptions
+            WHERE
+              status = 'active'
+          )
+          AND text IS NULL
+      ),
+      rand_selection AS (
+        SELECT
+          CEIL(
+            RANDOM() * (
+              SELECT
+                MAX(id)
+              FROM
+                valid_ads
+            )
+          ) AS rand_id
+      )
+    SELECT
+      *
+    FROM
+      valid_ads
+    WHERE
+      id >= (
+        SELECT
+          rand_id
+        FROM
+          rand_selection
+      )
+      OR id <= (
+        SELECT
+          rand_id
+        FROM
+          rand_selection
+      )
+    ORDER BY
+      CASE
+        WHEN id >= (
+          SELECT
+            rand_id
+          FROM
+            rand_selection
+        ) THEN 1
+        ELSE 2
+      END,
+      id
+    LIMIT
+      1;
+  `;
+  const result = await query;
+  const ad = result[0];
 
-    if (!ad) {
-      return res.status(204).send();
-    }
+  if (!ad) {
+    return res.status(204).send();
+  }
 
-    const desktopImage =
-      type === 'center' ? ad.mobile_image : ad.desktop_image_right;
+  const desktopImage =
+    type === 'center' ? ad.mobile_image : ad.desktop_image_right;
 
-    const desktopUrl = config.awsPublicUrl + '/' + desktopImage;
-    const mobileUrl = config.awsPublicUrl + '/' + ad.mobile_image;
+  const desktopUrl = config.awsPublicUrl + '/' + desktopImage;
+  const mobileUrl = config.awsPublicUrl + '/' + ad.mobile_image;
 
-    const impressionUrl = `${baseUrl}/v1/track/impression/${ad.id}`;
-    const clickUrl = `${baseUrl}/v1/track/click/${ad.id}`;
+  const impressionUrl = `${baseUrl}/v1/track/impression/${ad.id}`;
+  const clickUrl = `${baseUrl}/v1/track/click/${ad.id}`;
 
-    const adCode = `<a href="${clickUrl}" rel="nofollow">
+  const adCode = `<a href="${clickUrl}" rel="nofollow">
                           <img class="ad-image rounded-lg" src="${desktopUrl}" alt="Advertisement">
                       </a>
                       <img src="${impressionUrl}" style="display:none">
@@ -104,198 +102,181 @@ const getApprovedAds = catchAsync(async (req: Request, res: Response) => {
                           }
                       </style>`;
 
-    return res.header('Content-Type', 'text/html').send(adCode);
-  } catch (error) {
-    logger.error(error);
-    return null;
-  }
+  return res.header('Content-Type', 'text/html').send(adCode);
 });
 
 const getApprovedTextAds = catchAsync(async (_req: Request, res: Response) => {
-  try {
-    const query = userSql<Campaign[]>`
-      WITH
-        valid_ads AS (
-          SELECT
-            *
-          FROM
-            campaign__ads
-          WHERE
-            is_approved = TRUE
-            AND is_active = TRUE
-            AND api_subscription_id IN (
-              SELECT
-                id
-              FROM
-                api__subscriptions
-              WHERE
-                status = 'active'
-            )
-            AND text IS NOT NULL
-        ),
-        rand_selection AS (
-          SELECT
-            CEIL(
-              RANDOM() * (
-                SELECT
-                  MAX(id)
-                FROM
-                  valid_ads
-              )
-            ) AS rand_id
-        )
-      SELECT
-        *
-      FROM
-        valid_ads
-      WHERE
-        id >= (
-          SELECT
-            rand_id
-          FROM
-            rand_selection
-        )
-        OR id <= (
-          SELECT
-            rand_id
-          FROM
-            rand_selection
-        )
-      ORDER BY
-        CASE
-          WHEN id >= (
+  const query = userSql<Campaign[]>`
+    WITH
+      valid_ads AS (
+        SELECT
+          *
+        FROM
+          campaign__ads
+        WHERE
+          is_approved = TRUE
+          AND is_active = TRUE
+          AND api_subscription_id IN (
             SELECT
-              rand_id
+              id
             FROM
-              rand_selection
-          ) THEN 1
-          ELSE 2
-        END,
-        id
-      LIMIT
-        1;
-    `;
-    const result = await query;
-    const ad = result[0];
+              api__subscriptions
+            WHERE
+              status = 'active'
+          )
+          AND text IS NOT NULL
+      ),
+      rand_selection AS (
+        SELECT
+          CEIL(
+            RANDOM() * (
+              SELECT
+                MAX(id)
+              FROM
+                valid_ads
+            )
+          ) AS rand_id
+      )
+    SELECT
+      *
+    FROM
+      valid_ads
+    WHERE
+      id >= (
+        SELECT
+          rand_id
+        FROM
+          rand_selection
+      )
+      OR id <= (
+        SELECT
+          rand_id
+        FROM
+          rand_selection
+      )
+    ORDER BY
+      CASE
+        WHEN id >= (
+          SELECT
+            rand_id
+          FROM
+            rand_selection
+        ) THEN 1
+        ELSE 2
+      END,
+      id
+    LIMIT
+      1;
+  `;
+  const result = await query;
+  const ad = result[0];
 
-    if (!ad) {
-      return res.status(204).send();
-    }
+  if (!ad) {
+    return res.status(204).send();
+  }
 
-    const clickUrl = `${baseUrl}/v1/track/click/${ad.id}`;
-    const impressionUrl = `${baseUrl}/v1/track/impression/${ad.id}`;
-    let adCode = `<div class="ad-text-content" style="font-size: 14px;font-family: Manrope, sans-serif; text-align: left;">
+  const clickUrl = `${baseUrl}/v1/track/click/${ad.id}`;
+  const impressionUrl = `${baseUrl}/v1/track/impression/${ad.id}`;
+  let adCode = `<div class="ad-text-content" style="font-size: 14px;font-family: Manrope, sans-serif; text-align: left;">
                           <p><b>Sponsored: </b>
                           <img src="${impressionUrl}" style="display:none">`;
 
-    if (ad.icon) {
-      const iconUrl = config.awsPublicUrl + '/' + ad.icon;
-      adCode += `<img src="${iconUrl}" alt="Icon" class="ad-icon" style="display: inline;"> `;
-    }
+  if (ad.icon) {
+    const iconUrl = config.awsPublicUrl + '/' + ad.icon;
+    adCode += `<img src="${iconUrl}" alt="Icon" class="ad-icon" style="display: inline;"> `;
+  }
 
-    adCode += `&nbsp;<b>${ad.site_name}</b>: ${ad.text.replace(/\n/g, '<br>')} 
+  adCode += `&nbsp;<b>${ad.site_name}</b>: ${ad.text.replace(/\n/g, '<br>')} 
                    <a href="${clickUrl}" class="text-blue-500 dark:text-green-250 font-bold no-underline" target="_blank" rel="nofollow">${
                      ad.link_name
                    }</a>
                    </p></div>`;
 
-    return res.header('Content-Type', 'text/html').send(adCode);
-  } catch (error) {
-    logger.error(error);
-    return null;
-  }
+  return res.header('Content-Type', 'text/html').send(adCode);
 });
 
 const trackImpression = catchAsync(async (req: Request, res: Response) => {
   const { campaignId } = req.params;
-  try {
-    const now = new Date();
 
-    await userSql`
-      INSERT INTO
-        campaign_ad_tracking_datas (
-          INTERVAL,
-          campaign_ad_id,
-          impressions,
-          created_at,
-          updated_at
-        )
-      VALUES
-        (
-          ${now},
-          ${campaignId},
-          1,
-          ${now},
-          ${now}
-        )
-      ON CONFLICT (campaign_ad_id, INTERVAL) DO
-      UPDATE
-      SET
-        impressions = campaign_ad_tracking_datas.impressions + 1
-    `;
+  const now = new Date();
 
-    const transparentPixel = Buffer.from(
-      'iVBORw0KGgoAAAANSUhEUgAAAAUA\n' +
-        'AAAFCAYAAACNbyblAAAAHElEQVR42mJ8/5+hAQQw4f3QXAANuA4HM+DQAAAABJRU5ErkJggg==',
-      'base64',
-    );
+  await userSql`
+    INSERT INTO
+      campaign_ad_tracking_datas (
+        INTERVAL,
+        campaign_ad_id,
+        impressions,
+        created_at,
+        updated_at
+      )
+    VALUES
+      (
+        ${now},
+        ${campaignId},
+        1,
+        ${now},
+        ${now}
+      )
+    ON CONFLICT (campaign_ad_id, INTERVAL) DO
+    UPDATE
+    SET
+      impressions = campaign_ad_tracking_datas.impressions + 1
+  `;
 
-    res.set('Content-Type', 'image/png');
-    return res.status(200).send(transparentPixel);
-  } catch (error) {
-    logger.error(error);
-    return null;
-  }
+  const transparentPixel = Buffer.from(
+    'iVBORw0KGgoAAAANSUhEUgAAAAUA\n' +
+      'AAAFCAYAAACNbyblAAAAHElEQVR42mJ8/5+hAQQw4f3QXAANuA4HM+DQAAAABJRU5ErkJggg==',
+    'base64',
+  );
+
+  res.set('Content-Type', 'image/png');
+  return res.status(200).send(transparentPixel);
 });
 
 const trackClick = catchAsync(async (req: Request, res: Response) => {
   const { campaignId } = req.params;
-  try {
-    const now = new Date();
 
-    await userSql`
-      INSERT INTO
-        campaign_ad_tracking_datas (
-          INTERVAL,
-          campaign_ad_id,
-          clicks,
-          created_at,
-          updated_at
-        )
-      VALUES
-        (
-          ${now},
-          ${campaignId},
-          1,
-          ${now},
-          ${now}
-        )
-      ON CONFLICT (campaign_ad_id, INTERVAL) DO
-      UPDATE
-      SET
-        clicks = campaign_ad_tracking_datas.clicks + 1
-    `;
+  const now = new Date();
 
-    const adResult = await userSql`
-      SELECT
-        url
-      FROM
-        campaign__ads
-      WHERE
-        id = ${campaignId}
-    `;
+  await userSql`
+    INSERT INTO
+      campaign_ad_tracking_datas (
+        INTERVAL,
+        campaign_ad_id,
+        clicks,
+        created_at,
+        updated_at
+      )
+    VALUES
+      (
+        ${now},
+        ${campaignId},
+        1,
+        ${now},
+        ${now}
+      )
+    ON CONFLICT (campaign_ad_id, INTERVAL) DO
+    UPDATE
+    SET
+      clicks = campaign_ad_tracking_datas.clicks + 1
+  `;
 
-    if (!adResult.length) {
-      return res.status(404).json({ error: 'Ad not found.' });
-    }
-    const targetUrl = adResult[0].url;
+  const adResult = await userSql`
+    SELECT
+      url
+    FROM
+      campaign__ads
+    WHERE
+      id = ${campaignId}
+  `;
 
-    res.setHeader('Location', targetUrl);
-    return res.status(302).send();
-  } catch (error) {
-    logger.error(error);
-    return null;
+  if (!adResult.length) {
+    return res.status(404).json({ error: 'Ad not found.' });
   }
+  const targetUrl = adResult[0].url;
+
+  res.setHeader('Location', targetUrl);
+  return res.status(302).send();
 });
 
 export default {
