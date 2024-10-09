87b98d0c81c09daa58dc9e75ec75c0032584993b
alexandra <reese2k24@gmail.com>
2024-10-09 12:04:36 +0530

    fix(api): update route and package version

--- apps/api/src/routes/campaigns.ts
+++ apps/api/src/routes/campaigns.ts
@@ -7,35 +7,15 @@ const route = Router();
 const routes = (app: Router) => {
   app.use('/', route);
 
-  /**
-   * @ignore
-   * GET /v1/campaigns
-   * @return 200 - success response
-   */
   route.get('/campaigns', campaigns.getApprovedAds);
 
-  /**
-   * @ignore
-   * GET /v1/campaigns/text-ads
-   * @return 200 - success response
-   */
   route.get('/campaigns/text-ads', campaigns.getApprovedTextAds);
 
-  /**
-   * @ignore
-   * GET /v1/campaigns/track/impression/:campaignId
-   * @return 200 - success response
-   */
   route.get(
     '/campaigns/track/impression/:campaignId',
     campaigns.trackImpression,
   );
 
-  /**
-   * @ignore
-   * GET /v1/campaigns/track/click/:campaignId
-   * @return 200 - success response
-   */
   route.get('/campaigns/track/click/:campaignId', campaigns.trackClick);
 };
 
