c8e095ae4245bc5e5d87269bdddb5a6f8de77755
kevin-devstack <techsavvy1815@gmail.com>
2024-10-25 18:40:15 +0530

    feat(app): migrate about page to app router

---
+++ apps/app/src/app/[locale]/about/page.tsx
@@ -0,0 +1,24 @@
+import { getTranslations } from 'next-intl/server';
+
+export default async function About({
+  params: { locale },
+}: {
+  params: { locale: string };
+}) {
+  const t = await getTranslations({ locale });
+  return (
+    <>
+      <div className="bg-hero-pattern dark:bg-hero-pattern-dark h-72 -mb-48"></div>
+      <div className="py-16 bg-white dark:bg-black-600 soft-shadow sm:container sm:mx-auto rounded-md my-10">
+        <h1 className="mb-4 pt-8 sm:text-2xl text-center text-2xl text-green-500 dark:text-green-250">
+          {t('About Nearblocks')}
+        </h1>
+        <div className="text-base text-neargray-600 dark:text-neargray-10 py-8  mx-10 text-center">
+          {t(
+            'NearBlocks is the leading Blockchain Explorer, Search, API and Analytics Platform for Near Protocol, a decentralized smart contracts platform. Built and launched in 2022, it is one of the earliest projects built around Near Protocol and its community with the mission of providing equitable access to blockchain data.',
+          )}
+        </div>
+      </div>
+    </>
+  );
+}
