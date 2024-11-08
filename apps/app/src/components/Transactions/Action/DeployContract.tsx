23dadadec9d98bee331fff3b6bbf601c72887d43
Yudho <149754501+yudhomax@users.noreply.github.com>
2024-11-08 14:52:12 +0530

    feat(app): add link to receipt details in transaction action (#612)
    feat(app): display receipt details attached with transaction actions

--- apps/app/src/components/Transactions/Action/DeployContract.tsx
+++ apps/app/src/components/Transactions/Action/DeployContract.tsx
@@ -1,27 +1,26 @@
+import FaRight from '@/components/Icons/FaRight';
 import { shortenAddress } from '@/utils/libs';
 import { ActionPropsInfo } from '@/utils/types';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 const DeployContract = (props: ActionPropsInfo) => {
-  const FaRight = (props: { className: string }) => {
-    return (
-      <svg
-        stroke="currentColor"
-        fill="currentColor"
-        stroke-width="0"
-        viewBox="0 0 192 512"
-        className={props.className}
-        height="1em"
-        width="1em"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path d="M0 384.662V127.338c0-17.818 21.543-26.741 34.142-14.142l128.662 128.662c7.81 7.81 7.81 20.474 0 28.284L34.142 398.804C21.543 411.404 0 402.48 0 384.662z"></path>
-      </svg>
-    );
+  const router = useRouter();
+  const { hash } = router.query;
+
+  const handleClick = () => {
+    router.push(`/txns/${hash}#execution#${props.action?.receiptId}`);
   };
+
   return (
     <div className="action flex flex-wrap items-center break-all leading-7">
-      <FaRight className="inline-flex text-gray-400 dark:text-neargray-10 text-xs" />
+      {props?.action?.receiptId && hash ? (
+        <span onClick={handleClick} className="cursor-pointer">
+          <FaRight className="inline-flex text-gray-400 dark:text-neargray-10 text-xs" />
+        </span>
+      ) : (
+        <FaRight className="inline-flex text-gray-400 dark:text-neargray-10 text-xs" />
+      )}
       <span className="font-bold px-1">
         Deploy Contract{' '}
         <Link
--- apps/app/src/components/Transactions/Action/Wrap/WrapDeposit.tsx
+++ apps/app/src/components/Transactions/Action/Wrap/WrapDeposit.tsx
@@ -1,22 +1,14 @@
 import TokenInfo from '@/components/common/TokenInfo';
+import FaRight from '@/components/Icons/FaRight';
 import { EventPropsInfo } from '@/utils/types';
+import { useRouter } from 'next/router';
 
 const WrapDeposit = (props: EventPropsInfo) => {
-  const FaRight = (props: { className: string }) => {
-    return (
-      <svg
-        stroke="currentColor"
-        fill="currentColor"
-        stroke-width="0"
-        viewBox="0 0 192 512"
-        className={props.className}
-        height="1em"
-        width="1em"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path d="M0 384.662V127.338c0-17.818 21.543-26.741 34.142-14.142l128.662 128.662c7.81 7.81 7.81 20.474 0 28.284L34.142 398.804C21.543 411.404 0 402.48 0 384.662z"></path>
-      </svg>
-    );
+  const router = useRouter();
+  const { hash } = router.query;
+
+  const handleClick = () => {
+    router.push(`/txns/${hash}#execution#${props.event?.receiptId}`);
   };
 
   const log = props.event.logs?.match(/^Deposit (\d+) NEAR to ([\S]+)/);
@@ -25,7 +17,13 @@ const WrapDeposit = (props: EventPropsInfo) => {
 
   return (
     <div className="action flex flex-wrap items-center break-all leading-7">
-      <FaRight className="inline-flex text-gray-400 text-xs" />
+      {props?.event?.receiptId && hash ? (
+        <span onClick={handleClick} className="cursor-pointer">
+          <FaRight className="inline-flex text-gray-400 text-xs" />
+        </span>
+      ) : (
+        <FaRight className="inline-flex text-gray-400 text-xs" />
+      )}
       <span className="font-bold px-1">Mint </span>
 
       <TokenInfo contract={props.event.contract} amount={log[1]} />
