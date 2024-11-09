722b4a9a495d7b8b639a2f94b772638a9eb25fa1
Andrew Lau <andrew@andrewklau.com>
2024-11-09 12:51:29 +0700

    fix(app-lite): update footer links

--- apps/app-lite/src/components/Footer.tsx
+++ apps/app-lite/src/components/Footer.tsx
@@ -1,32 +1,24 @@
-import { useNetworkStore } from '@/stores/network';
-
 import Github from './Icons/Github';
 
 const Footer = () => {
-  const network = useNetworkStore((state) => state.network);
-
   return (
     <div className="bg-bottom-right">
       <div className="bg-bottom-left">
         <div className="container mx-auto px-3 pb-16">
           <div className="flex justify-between border-t border-border-body">
             <div className="font-light text-xs text-text-body pb-1 pt-6 text-center">
-              Powered by{' '}
+              Built on BOS by {' '}
               <a
                 className="text-primary"
-                href={`${
-                  network === 'testnet'
-                    ? 'https://testnet.nearblocks.io/'
-                    : 'https://nearblocks.io'
-                }`}
+                href="https://nearblocks.io"
                 target="_blank"
               >
-                NearBlocks.io
+                NearBlocks
               </a>
             </div>
             <a
               className="pb-1 pt-5"
-              href="https://github.com/Nearblocks"
+              href="https://github.com/Nearblocks/nearblocks/tree/main/apps/app-lite"
               rel="noreferrer nofollow noopener"
               target="_blank"
             >
