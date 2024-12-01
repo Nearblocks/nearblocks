0d88f82dc1f2786c6c2426dc7af15929e2cc6c9e
Andrew Lau <andrew@andrewklau.com>
2024-12-01 11:50:35 +0700

    feat(app): add baselime

---
+++ apps/app/instrumentation.ts
@@ -0,0 +1,20 @@
+export async function register() {
+  if (process.env.NEXT_RUNTIME === 'nodejs') {
+    const { BaselimeSDK, BetterHttpInstrumentation, VercelPlugin } =
+      await import('@baselime/node-opentelemetry');
+
+    const sdk = new BaselimeSDK({
+      instrumentations: [
+        new BetterHttpInstrumentation({
+          plugins: [
+            // Add the Vercel plugin to enable correlation between your logs and traces for projects deployed on Vercel
+            new VercelPlugin(),
+          ],
+        }),
+      ],
+      serverless: true,
+    });
+
+    sdk.start();
+  }
+}
