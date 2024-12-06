bc30c39e0d46861d5f98b4f9593ae07fe36f9115
Andrew Lau <andrew@andrewklau.com>
2024-12-06 18:51:01 +0700

    fix(app): use vercel otel

--- apps/app/instrumentation.ts
+++ apps/app/instrumentation.ts
@@ -1,24 +1,5 @@
-import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
-import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
-import { Resource } from '@opentelemetry/resources';
-import * as opentelemetry from '@opentelemetry/sdk-node';
-import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
-
-export async function register() {
-  if (process.env.NEXT_RUNTIME === 'nodejs') {
-    const sdk = new opentelemetry.NodeSDK({
-      instrumentations: [getNodeAutoInstrumentations()],
-      resource: new Resource({
-        [SemanticResourceAttributes.SERVICE_NAME]: 'nearblocks-app',
-      }),
-      traceExporter: new OTLPTraceExporter({
-        headers: {
-          'x-api-key': process.env.BASELIME_API_KEY || '',
-        },
-        url: 'https://otel.baselime.io/v1/traces',
-      }),
-    });
-
-    sdk.start();
-  }
+import { registerOTel } from '@vercel/otel'
+ 
+export function register() {
+  registerOTel({ serviceName: 'nearblocks-app' })
 }
