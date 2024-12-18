615a8606856955c4eab20823212031166c9e6ab4
kevin-devstack <techsavvy1815@gmail.com>
2024-12-18 17:48:41 +0530

    fix(app): NEP-245 token transactions

--- apps/app/src/components/common/NEPTokenTransactions.tsx
+++ apps/app/src/components/common/NEPTokenTransactions.tsx
@@ -5,19 +5,17 @@ import FaRight from '../Icons/FaRight';
 import { shortenAddress } from '@/utils/libs';
 import TokenInfo from './TokenInfo';
 import { TransactionLog } from '@/utils/types';
+
 interface ParsedEventListProps {
   events: TransactionLog[];
-  ftsEvents: any;
 }
 
 const NEPTokenTransactions: React.FC<ParsedEventListProps> = ({
-  ftsEvents,
   events,
 }: ParsedEventListProps) => {
   return (
     <>
       {events &&
-        ftsEvents &&
         events?.map((event: any, index: number) => {
           const parsedEvent: any = parseEventLogs(event);
           if (
@@ -25,64 +23,64 @@ const NEPTokenTransactions: React.FC<ParsedEventListProps> = ({
             (parsedEvent?.standard === 'nep141' &&
               Array.isArray(parsedEvent?.data))
           ) {
-            return parsedEvent?.data?.map((data: any, j: any) => {
-              const apiTokenInfo = ftsEvents?.find((ft: any) => {
-                return ft?.ft_meta?.contract === event?.contract;
-              });
-              const contract =
-                Array?.isArray(data?.token_ids) && data?.token_ids?.length > 0
-                  ? data?.token_ids?.[0]?.split(':')[1]
-                  : '';
-              const amount =
-                Array.isArray(data?.amounts) && data?.amounts?.length > 0
-                  ? data?.amounts[0]
-                  : '0';
+            return parsedEvent?.data?.flatMap((data: any, j: number) => {
+              const contracts = Array.isArray(data?.token_ids)
+                ? data?.token_ids.map(
+                    (tokenId: string) => tokenId.split(':')[1],
+                  )
+                : data
+                ? [event?.contract]
+                : [];
 
-              return (
-                <div className="flex" key={`${index}-${j}`}>
+              const amounts = Array.isArray(data?.amounts)
+                ? data?.amounts
+                : data?.amount
+                ? [data?.amount]
+                : ['0'];
+
+              return contracts?.map((contract: string, tokenIndex: number) => (
+                <div className="flex" key={`${index}-${j}-${tokenIndex}`}>
                   <div className="flex items-center flex-wrap break-all leading-7">
                     <FaRight className="inline-flex text-gray-400 text-xs" />
-                    {['mt_mint'].includes(parsedEvent?.event) ||
-                    ['MINT'].includes(apiTokenInfo?.cause) ? (
+                    {['mt_mint'].includes(parsedEvent?.event) ? (
                       <>
                         <div className="font-semibold text-gray px-1">
-                          From{' '}
+                          From
                           {'old_owner_id' in data && data?.old_owner_id ? (
                             <Link
                               href={`/address/${data.old_owner_id}`}
                               className="text-green-500 dark:text-green-250 font-normal pl-1 hover:no-underline"
                             >
-                              {shortenAddress(data.old_owner_id)}
+                              {shortenAddress(data?.old_owner_id)}
                             </Link>
                           ) : (
                             <span className="font-normal pl-1">system</span>
                           )}
                         </div>
                         <div className="font-semibold text-gray px-1">
-                          To{' '}
+                          To
                           {'owner_id' in data && data?.owner_id ? (
                             <Link
-                              href={`/address/${data.owner_id}`}
+                              href={`/address/${data?.owner_id}`}
                               className="text-green-500 dark:text-green-250 font-normal pl-1"
                             >
-                              {shortenAddress(data.owner_id)}
+                              {shortenAddress(data?.owner_id)}
                             </Link>
                           ) : (
                             <span className="font-normal pl-1">system</span>
                           )}
                         </div>
                       </>
-                    ) : ['mt_burn'].includes(parsedEvent.event) ||
-                      ['BURN'].includes(apiTokenInfo?.cause) ? (
+                    ) : ['mt_burn'].includes(parsedEvent.event) ? (
                       <>
                         <div className="font-semibold text-gray px-1">
                           From{' '}
                           {'owner_id' in data && data?.owner_id ? (
                             <Link
                               className="text-green-500 dark:text-green-250 font-normal pl-1 hover:no-underline"
-                              href={`/address/${data.owner_id}`}
+                              href={`/address/${data?.owner_id}`}
                             >
-                              {shortenAddress(data.owner_id)}
+                              {shortenAddress(data?.owner_id)}
                             </Link>
                           ) : (
                             <span className="font-normal pl-1">system</span>
@@ -93,9 +91,9 @@ const NEPTokenTransactions: React.FC<ParsedEventListProps> = ({
                           {'old_owner_id' in data && data?.old_owner_id ? (
                             <Link
                               className="text-green-500 dark:text-green-250 font-normal pl-1"
-                              href={`/address/${data.old_owner_id}`}
+                              href={`/address/${data?.old_owner_id}`}
                             >
-                              {shortenAddress(data.old_owner_id)}
+                              {shortenAddress(data?.old_owner_id)}
                             </Link>
                           ) : (
                             <span className="font-normal pl-1">system</span>
@@ -105,26 +103,26 @@ const NEPTokenTransactions: React.FC<ParsedEventListProps> = ({
                     ) : (
                       <>
                         <div className="font-semibold text-gray px-1">
-                          From{' '}
+                          From
                           {'old_owner_id' in data && data?.old_owner_id ? (
                             <Link
-                              href={`/address/${data.old_owner_id}`}
+                              href={`/address/${data?.old_owner_id}`}
                               className="text-green-500 dark:text-green-250 font-normal pl-1 hover:no-underline"
                             >
-                              {shortenAddress(data.old_owner_id)}
+                              {shortenAddress(data?.old_owner_id)}
                             </Link>
                           ) : (
                             <span className="font-normal pl-1">system</span>
                           )}
                         </div>
                         <div className="font-semibold text-gray px-1">
-                          To{' '}
+                          To
                           {'new_owner_id' in data && data?.new_owner_id ? (
                             <Link
-                              href={`/address/${data.new_owner_id}`}
+                              href={`/address/${data?.new_owner_id}`}
                               className="text-green-500 dark:text-green-250 font-normal pl-1"
                             >
-                              {shortenAddress(data.new_owner_id)}
+                              {shortenAddress(data?.new_owner_id)}
                             </Link>
                           ) : (
                             <span className="font-normal pl-1">system</span>
@@ -133,18 +131,17 @@ const NEPTokenTransactions: React.FC<ParsedEventListProps> = ({
                       </>
                     )}
                     <div className="flex items-center font-semibold text-gray px-1">
-                      For{' '}
+                      For
                       <span className="flex items-center pl-1 font-normal">
                         <TokenInfo
-                          apiTokenInfo={apiTokenInfo}
                           contract={contract}
-                          amount={amount}
+                          amount={amounts[tokenIndex] || '0'}
                         />
                       </span>
                     </div>
                   </div>
                 </div>
-              );
+              ));
             });
           }
           return null;
