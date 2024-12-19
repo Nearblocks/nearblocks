04e4983c51131b62e6745a1f5580b13c826fcd18
Yudho <149754501+yudhomax@users.noreply.github.com>
2024-12-19 11:52:05 +0530

    feat(app): add growth mate ads in transaction details page (#673)

---
+++ apps/app/src/components/app/Ad.tsx
@@ -0,0 +1,56 @@
+import React, { useEffect } from 'react';
+
+declare global {
+  interface Window {
+    growthmate: {
+      register: (unitId: string) => void;
+      unregister: (unitId: string) => void;
+    };
+  }
+}
+
+interface IAd {
+  accountId?: string;
+  className?: string;
+  format: string;
+  network?: string;
+  unitId: string;
+}
+
+const Ad: React.FC<IAd> = ({
+  accountId,
+  className,
+  format,
+  network,
+  unitId,
+}) => {
+  useEffect(() => {
+    if (window.growthmate !== undefined) window.growthmate.register(unitId);
+
+    let script: HTMLScriptElement | null =
+      document.querySelector('#gm-ad-script');
+    if (!script) {
+      script = document.createElement('script');
+      script.src = '/scripts/ad-unit-manager.react.js';
+      script.id = 'gm-ad-script';
+      document.head.appendChild(script);
+    }
+
+    script.addEventListener('load', () => window.growthmate.register(unitId));
+
+    return () => window.growthmate?.unregister(unitId);
+  }, [unitId]);
+
+  return (
+    <a
+      className={`gm-ad-unit ${className ?? ''}`}
+      data-gm-account-id={accountId ?? null}
+      data-gm-format={format}
+      data-gm-id={unitId}
+      data-gm-network={network ?? null}
+    ></a>
+  );
+};
+
+export { Ad };
+export type { IAd };
