94c0f8f1a5391359cac3440f16261b83a4699b08
kevin-devstack <techsavvy1815@gmail.com>
2025-02-07 11:30:06 +0530

    fix(app): remove rpc parameter from api requests on beta (#798)
    Co-authored-by: yudho <yadudevpro@gmail.com>

--- apps/app/src/components/app/Address/Contract/Overview.tsx
+++ apps/app/src/components/app/Address/Contract/Overview.tsx
@@ -1,8 +1,5 @@
 // import dynamic from 'next/dynamic';
-import { cookies } from 'next/headers';
-
 import { getRequest } from '@/utils/app/api';
-import { RpcProviders } from '@/utils/app/rpc';
 import { ContractCodeInfo } from '@/utils/types';
 
 import OverviewActions from './OverviewActions';
@@ -11,17 +8,11 @@ const Overview = async ({ id, searchParams }: any) => {
   const options: RequestInit = {
     next: { revalidate: 10 },
   };
-  const cookieStore = await cookies();
-  const rpcUrl = cookieStore.get('rpcUrl')?.value || RpcProviders?.[0]?.url;
 
   const [data, parse, account] = await Promise.all([
-    getRequest(
-      `account/${id}/contract/deployments?rpc=${rpcUrl}`,
-      searchParams,
-      options,
-    ),
-    getRequest(`account/${id}/contract/parse?rpc=${rpcUrl}`, {}, options),
-    getRequest(`account/${id}?rpc=${rpcUrl}`, {}, options),
+    getRequest(`account/${id}/contract/deployments`, searchParams, options),
+    getRequest(`account/${id}/contract/parse`, {}, options),
+    getRequest(`account/${id}`, {}, options),
   ]);
 
   return (
--- apps/app/src/components/app/NodeExplorer/NodeList.tsx
+++ apps/app/src/components/app/NodeExplorer/NodeList.tsx
@@ -1,17 +1,11 @@
-import { cookies } from 'next/headers';
 import QueryString from 'qs';
 
 import { getRequest } from '@/utils/app/api';
-import { RpcProviders } from '@/utils/app/rpc';
-
 import NodeListActions from './NodeListActions';
 
 export default async function NodeList({ searchParams }: any) {
-  const cookieStore = await cookies();
-  const rpcUrl = cookieStore.get('rpcUrl')?.value || RpcProviders?.[0]?.url;
-
   const data = await getRequest(
-    `validators?${QueryString.stringify(searchParams)}&rpc=${rpcUrl}`,
+    `validators?${QueryString.stringify(searchParams)}`,
   );
   const statsDetails = await getRequest('stats');
   const latestBlock = await getRequest('blocks/latest?limit=1');
