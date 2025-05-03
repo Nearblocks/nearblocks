16d702d420c17c460a621ab8cfcc1e1ce4654090
kevin-devstack <techsavvy1815@gmail.com>
2025-05-03 14:16:26 +0530

    fix(app): access client network config on method execution

--- apps/app/src/hooks/app/useWallet.ts
+++ apps/app/src/hooks/app/useWallet.ts
@@ -1,11 +1,11 @@
 import { useEffect, useState } from 'react';
 
 import { Wallet } from '@/components/app/wallet/near';
-import { networkId } from '@/utils/app/config';
+import { useConfig } from './useConfig';
 
 const useWallet = (): null | Wallet => {
   const [wallet, setWallet] = useState<null | Wallet>(null);
-
+  const { networkId } = useConfig();
   useEffect(() => {
     const initializeWallet = async () => {
       try {
