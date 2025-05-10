5c77f066eddd123da4691be9a0d7ee4f67cd5b3d
Andrew Lau <andrew@andrewklau.com>
2025-05-10 12:39:37 +0700

    Revert "fix(aggregates): temp increase limits"
    This reverts commit 64d3a44e4c95cf867f22627178727992bfe12ce5.

--- apps/aggregates/src/libs/utils.ts
+++ apps/aggregates/src/libs/utils.ts
@@ -2,8 +2,14 @@ export const NS_IN_S = 1_000_000_000;
 
 export const getLimit = (block: number) => {
   switch (true) {
-    case block < 196_212_773:
-      return 200;
+    case block < 60_000_000:
+      return 1_000;
+    case block < 70_000_000:
+      return 100;
+    case block < 90_000_000:
+      return 10;
+    case block < 110_000_000:
+      return 2;
     default:
       return 1;
   }
