8e11300d097ceb850a592a8f256426c9674f8913
Andrew Lau <andrew@andrewklau.com>
2025-06-07 13:00:37 +0700

    fix(indexer-base): tablespace name conventions

--- apps/indexer-base/migrations/20250606170810_tablespace.up.sql
+++ apps/indexer-base/migrations/20250606170810_tablespace.up.sql
@@ -1,52 +1,52 @@
 SELECT
   attach_tablespace (
-    'action-receipt-actions-tbs1',
+    'action_receipt_actions_tbs1',
     'action_receipt_actions',
     if_not_attached => true
   );
 
 SELECT
   attach_tablespace (
-    'action-receipt-actions-tbs2',
+    'action_receipt_actions_tbs2',
     'action_receipt_actions',
     if_not_attached => true
   );
 
 SELECT
   attach_tablespace (
-    'receipts-tbs1',
+    'receipts_tbs1',
     'receipts',
     if_not_attached => true
   );
 
 SELECT
   attach_tablespace (
-    'receipts-tbs2',
+    'receipts_tbs2',
     'receipts',
     if_not_attached => true
   );
 
 SELECT
   attach_tablespace (
-    'execution-outcomes-tbs1',
+    'execution_outcomes_tbs1',
     'execution_outcomes',
     if_not_attached => true
   );
 
 SELECT
   attach_tablespace (
-    'execution-outcomes-tbs2',
+    'execution_outcomes_tbs2',
     'execution_outcomes',
     if_not_attached => true
   );
 
 SELECT
   attach_tablespace (
-    'transactions-tbs1',
+    'transactions_tbs1',
     'transactions',
     if_not_attached => true
   );
 
 ALTER TABLE execution_outcome_receipts
 SET
-  TABLESPACE 'execution-outcome-receipts-tbs1';
+  TABLESPACE 'execution_outcome_receipts_tbs1';
