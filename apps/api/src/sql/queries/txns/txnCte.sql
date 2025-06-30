4df31ba9cdbd8f3ee877d9bdb6871ed080833f21
Ch3rry B@ry <cherrybary21@gmail.com>
2025-06-30 12:26:37 +0530

    feat(api): v3 txns apis

---
+++ apps/api/src/sql/queries/txns/txnCte.sql
@@ -0,0 +1,19 @@
+txn_selected AS (
+  SELECT
+    t.block_timestamp,
+    t.shard_id,
+    t.index_in_chunk,
+    t.transaction_hash,
+    t.receiver_account_id,
+    t.signer_account_id,
+    t.included_in_block_hash,
+    t.converted_into_receipt_id,
+    t.receipt_conversion_tokens_burnt,
+    t.receipt_conversion_gas_burnt
+  FROM
+    transactions t
+  WHERE
+    t.transaction_hash = ${hash}
+    AND t.block_timestamp >= ${start} -- rolling window start
+    AND t.block_timestamp <= ${end} -- rolling window end
+)
