50c4b04889a018615aee2003386d08dc10cf137f
kevin-devstack <techsavvy1815@gmail.com>
2025-07-11 15:24:35 +0530

    fix(explorer-selector): remove bos and migrate to page router

--- apps/explorer-selector/src/pages/[...slug].tsx
+++ apps/explorer-selector/src/pages/[...slug].tsx
@@ -1,5 +1,12 @@
-import ExplorerSelector from '@/components/ExplorerSelector';
-
+import { Spinner } from '@/components/lib/Spinner';
+import dynamic from 'next/dynamic';
+const ExplorerSelector = dynamic(
+  () => import('../components/ExplorerSelector'),
+  {
+    ssr: false,
+    loading: () => <Spinner />,
+  },
+);
 const ExplorerPage = () => {
   return <ExplorerSelector />;
 };
--- apps/explorer-selector/src/utils/config.ts
+++ apps/explorer-selector/src/utils/config.ts
@@ -1,15 +1,6 @@
-import type { Network, NetworkId } from './types';
 import { env } from 'next-runtime-env';
+import { NetworkId } from './types';
 
-export const networks: Record<NetworkId, Network> = {
-  mainnet: {
-    networkId: 'mainnet',
-  },
-  testnet: {
-    networkId: 'testnet',
-  },
+export const getNetworkId = (): NetworkId => {
+  return (env('NEXT_PUBLIC_NETWORK_ID') as NetworkId) || 'testnet';
 };
-
-export const networkId: NetworkId =
-  (env('NEXT_PUBLIC_BOS_NETWORK') as NetworkId) || 'testnet';
-export const network = networks[networkId];
