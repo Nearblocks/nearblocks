0ca41e70c7e7c77fc5ea0d2effb0e173cf6713b0
Cherry Bary <84170175+cherrybarry@users.noreply.github.com>
2025-07-18 15:54:00 +0530

    feat(indexer): support multiple instances of multichain indexer (#1107)

--- apps/indexer-multichain/README.md
+++ apps/indexer-multichain/README.md
@@ -13,18 +13,27 @@ DATABASE_CA=
 DATABASE_CERT=
 DATABASE_KEY=
 ARBITRUM_RPC_URL=
+ARBITRUM_START_BLOCK=  # 235722005 (mainnet) | 66361040 (testnet)
 BASE_RPC_URL=
+BASE_START_BLOCK=  # 17538127 (mainnet) | 13048656 (testnet)
 BITCOIN_RPC_URL=
+BITCOIN_START_BLOCK=  # 853779 (mainnet) | 2869965 (testnet)
 BSC_RPC_URL=
+BSC_START_BLOCK=  # 40766572 (mainnet) | 42381856 (testnet)
 ETHEREUM_RPC_URL=
+ETHEREUM_START_BLOCK=  # 20379839 (mainnet) | 6370346 (testnet)
 GNOSIS_RPC_URL=
+GNOSIS_START_BLOCK=  # 35136644 (mainnet) | 1035904 (testnet)
 OPTIMISM_RPC_URL=
+OPTIMISM_START_BLOCK=  # 123133412 (mainnet) | 15031530 (testnet)
 POLYGON_RPC_URL=
+POLYGON_START_BLOCK=  # 59770449 (mainnet) | 9892923 (testnet)
 SOLANA_RPC_URL=
+SOLANA_START_BLOCK=  # 279492479 (mainnet) | 314013232 (testnet)
 SENTRY_DSN=
 ```
 
 ### Supported Contracts
 
 - Mainnet: v1.signer
-- Testnet: v1.signer-prod.testnet, v1.signer-dev.testnet
+- Testnet: v1.signer-prod.testnet
--- apps/indexer-multichain/src/config.ts
+++ apps/indexer-multichain/src/config.ts
@@ -1,4 +1,4 @@
-import { cleanEnv, str } from 'envalid';
+import { cleanEnv, num, str } from 'envalid';
 
 import { Network } from 'nb-types';
 
@@ -6,69 +6,78 @@ import { Config } from '#types/types';
 
 const env = cleanEnv(process.env, {
   ARBITRUM_RPC_URL: str({ default: '' }),
+  ARBITRUM_START_BLOCK: num({ default: 0 }),
   BASE_RPC_URL: str({ default: '' }),
+  BASE_START_BLOCK: num({ default: 0 }),
   BITCOIN_RPC_URL: str({ default: '' }),
+  BITCOIN_START_BLOCK: num({ default: 0 }),
   BSC_RPC_URL: str({ default: '' }),
+  BSC_START_BLOCK: num({ default: 0 }),
   DATABASE_CA: str({ default: '' }),
   DATABASE_CERT: str({ default: '' }),
   DATABASE_KEY: str({ default: '' }),
   DATABASE_URL: str(),
   ETHEREUM_RPC_URL: str({ default: '' }),
+  ETHEREUM_START_BLOCK: num({ default: 0 }),
   GNOSIS_RPC_URL: str({ default: '' }),
+  GNOSIS_START_BLOCK: num({ default: 0 }),
   NETWORK: str({
     choices: [Network.MAINNET, Network.TESTNET],
   }),
   OPTIMISM_RPC_URL: str({ default: '' }),
+  OPTIMISM_START_BLOCK: num({ default: 0 }),
   POLYGON_RPC_URL: str({ default: '' }),
+  POLYGON_START_BLOCK: num({ default: 0 }),
   SENTRY_DSN: str({ default: '' }),
   SOLANA_RPC_URL: str({ default: '' }),
+  SOLANA_START_BLOCK: num({ default: 0 }),
 });
 
 const config: Config = {
   chains: {
     ARBITRUM: {
       interval: 1000, // 1s
-      start: env.NETWORK === Network.MAINNET ? 235722005 : 66361040,
+      start: env.ARBITRUM_START_BLOCK,
       url: env.ARBITRUM_RPC_URL,
     },
     BASE: {
       interval: 5000, // 5s
-      start: env.NETWORK === Network.MAINNET ? 17538127 : 13048656,
+      start: env.BASE_START_BLOCK,
       url: env.BASE_RPC_URL,
     },
     BITCOIN: {
       interval: env.NETWORK === Network.MAINNET ? 600000 : 300000, // 10m/5m
-      start: env.NETWORK === Network.MAINNET ? 853779 : 2869965,
+      start: env.BITCOIN_START_BLOCK,
       url: env.BITCOIN_RPC_URL,
     },
     BSC: {
       interval: 10000, // 10s
-      start: env.NETWORK === Network.MAINNET ? 40766572 : 42381856,
+      start: env.BSC_START_BLOCK,
       url: env.BSC_RPC_URL,
     },
     ETHEREUM: {
       interval: 10000, // 10s
-      start: env.NETWORK === Network.MAINNET ? 20379839 : 6370346,
+      start: env.ETHEREUM_START_BLOCK,
       url: env.ETHEREUM_RPC_URL,
     },
     GNOSIS: {
       interval: 10000, // 10s
-      start: env.NETWORK === Network.MAINNET ? 35136644 : 1035904,
+      start: env.GNOSIS_START_BLOCK,
       url: env.GNOSIS_RPC_URL,
     },
     OPTIMISM: {
       interval: 5000, // 5s
-      start: env.NETWORK === Network.MAINNET ? 123133412 : 15031530,
+      start: env.OPTIMISM_START_BLOCK,
       url: env.OPTIMISM_RPC_URL,
     },
     POLYGON: {
       interval: 5000, // 5s
-      start: env.NETWORK === Network.MAINNET ? 59770449 : 9892923,
+      start: env.POLYGON_START_BLOCK,
       url: env.POLYGON_RPC_URL,
     },
     SOLANA: {
       interval: 2000, // 2s
-      start: env.NETWORK === Network.MAINNET ? 279492479 : 314013232,
+      start: env.SOLANA_START_BLOCK,
       url: env.SOLANA_RPC_URL,
     },
   },
