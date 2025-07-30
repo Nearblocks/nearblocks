65a45283572f9f5c41ace3d1261b0634e1110a37
Ch3rry B@ry <cherrybary21@gmail.com>
2025-07-30 18:05:35 +0530

    feat: add txn, block details in multichain txns api

--- packages/nb-schemas/src/multichain/response.ts
+++ packages/nb-schemas/src/multichain/response.ts
@@ -4,8 +4,15 @@ import { DestinationChain } from 'nb-types';
 
 import { responseSchema } from '../common.js';
 
+const block = v.object({
+  block_hash: v.optional(v.string()),
+  block_height: v.optional(v.string()),
+  block_timestamp: v.optional(v.string()),
+});
+
 const txnList = v.object({
   account_id: v.string(),
+  block,
   block_timestamp: v.string(),
   dest_address: v.nullable(v.string()),
   dest_chain: v.nullable(v.enum(DestinationChain)),
@@ -13,6 +20,7 @@ const txnList = v.object({
   event_index: v.number(),
   public_key: v.string(),
   receipt_id: v.string(),
+  transaction_hash: v.string(),
 });
 
 const txnCount = v.object({
@@ -20,7 +28,9 @@ const txnCount = v.object({
   count: v.string(),
 });
 
-const txnsResponse = responseSchema(v.array(v.omit(txnList, ['event_index'])));
+const txnsResponse = responseSchema(
+  v.array(v.omit(txnList, ['block_timestamp', 'event_index'])),
+);
 const txnsCountResponse = responseSchema(v.omit(txnCount, ['cost']));
 
 export type McTxns = v.InferOutput<typeof txnList>;
