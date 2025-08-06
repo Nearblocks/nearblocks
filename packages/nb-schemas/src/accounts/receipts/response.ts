db387c1454667ed11cc08ef7ea8971e5b94b7931
Ch3rry B@ry <cherrybary21@gmail.com>
2025-08-06 14:48:30 +0530

    feat: account txns, receipts apis

---
+++ packages/nb-schemas/src/accounts/receipts/response.ts
@@ -0,0 +1,65 @@
+import * as v from 'valibot';
+
+import { ActionKind } from 'nb-types';
+
+import { responseSchema } from '../../common.js';
+
+const action = v.object({
+  action: v.enum(ActionKind),
+  method: v.nullable(v.string()),
+});
+
+const actionsAgg = v.object({
+  deposit: v.string(),
+});
+
+const block = v.object({
+  block_hash: v.optional(v.string()),
+  block_height: v.optional(v.string()),
+  block_timestamp: v.optional(v.string()),
+});
+
+const outcome = v.object({
+  status: v.optional(v.boolean()),
+});
+
+const receiptsList = v.object({
+  actions: v.array(action),
+  actions_agg: actionsAgg,
+  block,
+  included_in_block_timestamp: v.string(),
+  index_in_chunk: v.number(),
+  outcome,
+  predecessor_account_id: v.string(),
+  receipt_id: v.string(),
+  receiver_account_id: v.string(),
+  shard_id: v.number(),
+  transaction_hash: v.string(),
+});
+
+const receiptCount = v.object({
+  cost: v.string(),
+  count: v.string(),
+});
+
+const receiptsResponse = responseSchema(
+  v.array(
+    v.omit(receiptsList, [
+      'included_in_block_timestamp',
+      'shard_id',
+      'index_in_chunk',
+    ]),
+  ),
+);
+const receiptCountResponse = responseSchema(v.omit(receiptCount, ['cost']));
+
+export type AccountReceipts = v.InferOutput<typeof receiptsList>;
+export type AccountReceiptCount = v.InferOutput<typeof receiptCount>;
+
+export type AccountReceiptsRes = v.InferOutput<typeof receiptsResponse>;
+export type AccountReceiptCountRes = v.InferOutput<typeof receiptCountResponse>;
+
+export default {
+  count: receiptCountResponse,
+  receipts: receiptsResponse,
+};
