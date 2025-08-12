a2f85f655f33b01498d9a13f4f2036a342b36c57
Cherry Bary <84170175+cherrybarry@users.noreply.github.com>
2025-08-12 21:33:44 +0530

    fix(indexer): update migrations (#1125)

--- apps/indexer-events/migrations/20250603115702_tablespace.up.sql
+++ apps/indexer-events/migrations/20250603115702_tablespace.up.sql
@@ -9,3 +9,15 @@ SELECT
 
 SELECT
   attach_tablespace ('tbs2', 'nft_events', if_not_attached => true);
+
+SELECT
+  attach_tablespace ('tbs3', 'ft_events', if_not_attached => true);
+
+SELECT
+  attach_tablespace ('tbs3', 'nft_events', if_not_attached => true);
+
+SELECT
+  attach_tablespace ('tbs4', 'ft_events', if_not_attached => true);
+
+SELECT
+  attach_tablespace ('tbs4', 'nft_events', if_not_attached => true);
