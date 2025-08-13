e711c60610abc24494181031b2c4f56dc03320ca
Andrew Lau <andrew@andrewklau.com>
2025-08-13 10:19:09 +0700

    fix(indexer-base): attach additional tablespaces

--- apps/indexer-base/migrations/20250606170810_tablespace.up.sql
+++ apps/indexer-base/migrations/20250606170810_tablespace.up.sql
@@ -14,6 +14,13 @@ SELECT
 
 SELECT
   attach_tablespace (
+    'action_receipt_actions_tbs3',
+    'action_receipt_actions',
+    if_not_attached => true
+  );
+
+SELECT
+  attach_tablespace (
     'receipts_tbs1',
     'receipts',
     if_not_attached => true
@@ -22,6 +29,12 @@ SELECT
 SELECT
   attach_tablespace (
     'receipts_tbs2',
+    'receipts',
+    if_not_attached => true
+  );
+SELECT
+  attach_tablespace (
+    'receipts_tbs3',
     'receipts',
     if_not_attached => true
   );
@@ -36,6 +49,13 @@ SELECT
 SELECT
   attach_tablespace (
     'execution_outcomes_tbs2',
+    'execution_outcomes',
+    if_not_attached => true
+  );
+
+SELECT
+  attach_tablespace (
+    'execution_outcomes_tbs3',
     'execution_outcomes',
     if_not_attached => true
   );
