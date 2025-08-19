ac566f33178d9b66e7acae3cf1db8c8d91170c06
Andrew Lau <andrew@andrewklau.com>
2025-08-19 09:25:39 +0700

    Revert "fix(indexer-base): attach additional tablespaces"
    This reverts commit e711c60610abc24494181031b2c4f56dc03320ca.

--- apps/indexer-base/migrations/20250606170810_tablespace.up.sql
+++ apps/indexer-base/migrations/20250606170810_tablespace.up.sql
@@ -14,13 +14,6 @@ SELECT
 
 SELECT
   attach_tablespace (
-    'action_receipt_actions_tbs3',
-    'action_receipt_actions',
-    if_not_attached => true
-  );
-
-SELECT
-  attach_tablespace (
     'receipts_tbs1',
     'receipts',
     if_not_attached => true
@@ -35,13 +28,6 @@ SELECT
 
 SELECT
   attach_tablespace (
-    'receipts_tbs3',
-    'receipts',
-    if_not_attached => true
-  );
-
-SELECT
-  attach_tablespace (
     'execution_outcomes_tbs1',
     'execution_outcomes',
     if_not_attached => true
@@ -50,13 +36,6 @@ SELECT
 SELECT
   attach_tablespace (
     'execution_outcomes_tbs2',
-    'execution_outcomes',
-    if_not_attached => true
-  );
-
-SELECT
-  attach_tablespace (
-    'execution_outcomes_tbs3',
     'execution_outcomes',
     if_not_attached => true
   );
