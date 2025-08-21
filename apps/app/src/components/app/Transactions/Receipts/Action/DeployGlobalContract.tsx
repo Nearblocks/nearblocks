a65ca8be751ed494d0d1dd4daf7abcd336bd5cfd
kevin-devstack <techsavvy1815@gmail.com>
2025-08-21 17:36:55 +0530

    feat(app): add global contract args support

--- apps/app/src/components/app/Transactions/Receipts/Action/DeployGlobalContract.tsx
+++ apps/app/src/components/app/Transactions/Receipts/Action/DeployGlobalContract.tsx
@@ -2,22 +2,68 @@ import FaCode from '@/components/app/Icons/FaCode';
 import { shortenAddress } from '@/utils/libs';
 import { TransactionActionInfo } from '@/utils/types';
 import { Link } from '@/i18n/routing';
+import FaMinimize from '@/components/app/Icons/FaMinimize';
+import FaExpand from '@/components/app/Icons/FaExpand';
+import { useState } from 'react';
 
 const DeployGlobalContract = (props: TransactionActionInfo) => {
-  const { receiver } = props;
+  const { receiver, args } = props;
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  function displayArgs(args: any) {
+    if (!args || typeof args === 'undefined') {
+      return 'The arguments are empty';
+    }
+
+    if (typeof args === 'string') {
+      return args;
+    } else if (typeof args === 'object') {
+      return JSON.stringify(args, null, 2);
+    }
+
+    return String(args);
+  }
+
+  const displayData = displayArgs(args);
 
   return (
-    <div className="py-1">
-      <FaCode className="inline-flex text-emerald-400 mr-1" />{' '}
-      {'Deploy Global Contract'} (
-      <Link
-        className="text-green-500 dark:text-green-250 font-bold hover:no-underline"
-        href={`/address/${receiver}`}
-      >
-        {shortenAddress(receiver)}
-      </Link>
-      ) {'deployed'}
-    </div>
+    <>
+      <div className="pt-1">
+        <FaCode className="inline-flex text-emerald-400 mr-1" />{' '}
+        {'Deploy Global Contract'} (
+        <Link
+          className="text-green-500 dark:text-green-250 font-bold hover:no-underline"
+          href={`/address/${receiver}`}
+        >
+          {shortenAddress(receiver)}
+        </Link>
+        ) {'deployed'}
+      </div>
+
+      <div className="relative w-full">
+        <div className="absolute top-2 sm:!mr-4 right-2 flex">
+          <button
+            onClick={() => setIsExpanded((prev) => !prev)}
+            className="bg-gray-700 dark:bg-gray-500 bg-opacity-10 hover:bg-opacity-100 group rounded-full p-1.5 w-7 h-7"
+          >
+            {!isExpanded ? (
+              <FaMinimize className="fill-current -z-50 text-gray-700 dark:text-neargray-10 group-hover:text-white h-4 w-4" />
+            ) : (
+              <FaExpand className="fill-current -z-50 text-gray-700 dark:text-neargray-10 group-hover:text-white h-4 w-4" />
+            )}
+          </button>
+        </div>
+        <div
+          className={`block appearance-none outline-none w-full border rounded-lg bg-gray-100 dark:bg-black-200 dark:border-black-200 p-3 resize-y font-space-mono whitespace-pre-wrap overflow-auto max-w-full overflow-x-auto ${
+            !isExpanded ? 'h-[8rem]' : ''
+          }`}
+        >
+          {typeof displayData === 'object'
+            ? JSON.stringify(displayData, null, 2)
+            : displayData}
+        </div>
+      </div>
+    </>
   );
 };
 
