532af40361a3dfc2656cec804832378a9639ab0b
kevin-devstack <techsavvy1815@gmail.com>
2025-08-22 17:19:15 +0530

    fix(app): replace default pending status with loading state

--- apps/app/src/components/app/Transactions/Details.tsx
+++ apps/app/src/components/app/Transactions/Details.tsx
@@ -529,7 +529,7 @@ const Details = (props: Props) => {
                 </Tooltip>
                 <div>{t ? t('txnDetails.status.text.0') : 'Status'}</div>
               </div>
-              {loading ? (
+              {!txn?.outcomes?.status && !rpcTxn?.status ? (
                 <div className="w-full md:w-3/4">
                   <Loader wrapperClassName="flex w-14" />
                 </div>
