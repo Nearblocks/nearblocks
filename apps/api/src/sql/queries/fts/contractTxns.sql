c8d7dd501cef9e2883a8ff81227205c86eb72a06
Ch3rry B@ry <cherrybary21@gmail.com>
2025-09-09 10:18:45 +0530

    feat: ft contract txn, nft txn, nft contract txn apis

---
+++ apps/api/src/sql/queries/fts/contractTxns.sql
@@ -0,0 +1,83 @@
+WITH
+  params AS (
+    SELECT
+      ${cursor.timestamp}::BIGINT AS block_timestamp,
+      ${cursor.shard}::SMALLINT AS shard_id,
+      ${cursor.type}::SMALLINT AS event_type,
+      ${cursor.index}::INTEGER AS event_index
+  )
+SELECT
+  ft.block_timestamp,
+  ft.shard_id,
+  ft.event_type,
+  ft.event_index,
+  ft.receipt_id,
+  ft.contract_account_id,
+  ft.affected_account_id,
+  ft.involved_account_id,
+  ft.cause,
+  ft.delta_amount,
+  m.meta
+FROM
+  ft_events ft
+  JOIN params p ON TRUE
+  JOIN LATERAL (
+    SELECT
+      JSONB_BUILD_OBJECT(
+        'contract',
+        contract,
+        'name',
+        name,
+        'symbol',
+        symbol,
+        'decimals',
+        decimals,
+        'icon',
+        icon,
+        'reference',
+        reference
+      ) AS meta
+    FROM
+      ft_meta fm
+    WHERE
+      fm.contract = ft.contract_account_id
+  ) m ON TRUE
+WHERE
+  (
+    p.block_timestamp IS NULL
+    OR (
+      ft.block_timestamp,
+      ft.shard_id,
+      ft.event_type,
+      ft.event_index
+    ) < (
+      p.block_timestamp,
+      p.shard_id,
+      p.event_type,
+      p.event_index
+    )
+  )
+  AND (
+    ${start}::BIGINT IS NULL
+    OR ft.block_timestamp > ${start}
+  )
+  AND (
+    ${end}::BIGINT IS NULL
+    OR ft.block_timestamp < ${end}
+  )
+  AND ft.contract_account_id = ${contract}
+  AND (
+    ${affected}::BIGINT IS NULL
+    OR ft.affected_account_id = ${affected}
+  )
+  AND (
+    ${involved}::BIGINT IS NULL
+    OR ft.involved_account_id = ${involved}
+  )
+ORDER BY
+  block_timestamp DESC,
+  shard_id DESC,
+  event_type DESC,
+  event_index DESC
+LIMIT
+  ${limit}
--- packages/nb-schemas/src/index.ts
+++ packages/nb-schemas/src/index.ts
@@ -3,5 +3,6 @@ export type * from './blocks/index.js';
 export type * from './fts/index.js';
 export type * from './keys/index.js';
 export type * from './multichain/index.js';
+export type * from './nfts/index.js';
 export type * from './search/index.js';
 export type * from './txns/index.js';
