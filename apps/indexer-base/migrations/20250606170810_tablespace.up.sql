2f426f4fa76b0fcc06d64c7aee4d335e26ddaaba
Andrew Lau <andrew@andrewklau.com>
2025-11-10 09:05:44 +0700

    fix(indexer-base): disable tablespaces

--- apps/indexer-base/migrations/20250606170810_tablespace.up.sql
+++ apps/indexer-base/migrations/20250606170810_tablespace.up.sql
@@ -1,52 +1,46 @@
-SELECT
-  attach_tablespace (
-    'action_receipt_actions_tbs1',
-    'action_receipt_actions',
-    if_not_attached => true
-  );
-
-SELECT
-  attach_tablespace (
-    'action_receipt_actions_tbs2',
-    'action_receipt_actions',
-    if_not_attached => true
-  );
-
-SELECT
-  attach_tablespace (
-    'receipts_tbs1',
-    'receipts',
-    if_not_attached => true
-  );
-
-SELECT
-  attach_tablespace (
-    'receipts_tbs2',
-    'receipts',
-    if_not_attached => true
-  );
-
-SELECT
-  attach_tablespace (
-    'execution_outcomes_tbs1',
-    'execution_outcomes',
-    if_not_attached => true
-  );
-
-SELECT
-  attach_tablespace (
-    'execution_outcomes_tbs2',
-    'execution_outcomes',
-    if_not_attached => true
-  );
-
-SELECT
-  attach_tablespace (
-    'transactions_tbs1',
-    'transactions',
-    if_not_attached => true
-  );
-
-ALTER TABLE execution_outcome_receipts
-SET
-  TABLESPACE execution_outcome_receipts_tbs1;
+-- Tablespace migration disabled
+-- SELECT
+--   attach_tablespace (
+--     'action_receipt_actions_tbs1',
+--     'action_receipt_actions',
+--     if_not_attached => true
+--   );
+-- SELECT
+--   attach_tablespace (
+--     'action_receipt_actions_tbs2',
+--     'action_receipt_actions',
+--     if_not_attached => true
+--   );
+-- SELECT
+--   attach_tablespace (
+--     'receipts_tbs1',
+--     'receipts',
+--     if_not_attached => true
+--   );
+-- SELECT
+--   attach_tablespace (
+--     'receipts_tbs2',
+--     'receipts',
+--     if_not_attached => true
+--   );
+-- SELECT
+--   attach_tablespace (
+--     'execution_outcomes_tbs1',
+--     'execution_outcomes',
+--     if_not_attached => true
+--   );
+-- SELECT
+--   attach_tablespace (
+--     'execution_outcomes_tbs2',
+--     'execution_outcomes',
+--     if_not_attached => true
+--   );
+-- SELECT
+--   attach_tablespace (
+--     'transactions_tbs1',
+--     'transactions',
+--     if_not_attached => true
+--   );
+-- ALTER TABLE execution_outcome_receipts
+-- SET
+--   TABLESPACE execution_outcome_receipts_tbs1;
