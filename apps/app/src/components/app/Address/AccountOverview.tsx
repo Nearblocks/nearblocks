23932bc61ff8ec7c267a27589095b0cf6da70d51
kevin-devstack <techsavvy1815@gmail.com>
2025-11-25 13:26:41 +0530

    feat(app): show RPC balance when balance indexer is out of sync

--- apps/app/src/components/app/Address/AccountOverview.tsx
+++ apps/app/src/components/app/Address/AccountOverview.tsx
@@ -7,9 +7,11 @@ import {
 
 export default async function AccountOverview({ id }: any) {
   const accountData = getRequest(`v1/account/${id}`);
+  const statsData = getRequest('v1/stats');
   const tokenData = getRequest(`v1/fts/${id}`);
   const inventoryData = getRequest(`v1/account/${id}/inventory`);
   const mtsData = getRequest(`v2/account/${id}/inventory/mts`);
+  const syncData = getRequest(`v1/sync/status`);
   const spamList = getRequest(
     'https://raw.githubusercontent.com/Nearblocks/spam-token-list/main/tokens.json',
     {},
@@ -39,9 +41,11 @@ export default async function AccountOverview({ id }: any) {
       inventoryDataPromise={inventoryData}
       mtsDataPromise={mtsData}
       spamTokensPromise={spamList}
+      statsDataPromise={statsData}
       tokenDataPromise={tokenData}
       intentsTokenPricesPromise={intentsTokenPrices}
       refTokenPricesPromise={refTokenPrices}
+      syncDataPromise={syncData}
     />
   );
 }
