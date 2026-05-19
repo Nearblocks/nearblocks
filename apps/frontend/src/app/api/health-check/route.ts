e2ee456ce5f696bd6edacfb5642d14996d345adb
Andrew Lau <andrew@andrewklau.com>
2026-05-19 13:34:06 +0700

    fix(frontend): healthcheck legacy endpoint (#1458)

--- apps/frontend/src/app/api/health-check/route.ts
+++ apps/frontend/src/app/api/health-check/route.ts
@@ -1,1 +1,13 @@
-export { dynamic, GET } from '../healthcheck/route';
+import { NextResponse } from 'next/server';
+
+export const dynamic = 'force-static';
+
+export async function GET() {
+  return NextResponse.json(
+    {
+      status: 'healthy',
+      timestamp: new Date().toISOString(),
+    },
+    { status: 200 },
+  );
+}
