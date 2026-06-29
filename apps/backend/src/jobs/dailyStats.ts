7664ac7d13c4daa1455667288e84c9a920ada53f
Bayu <bayulaksana479@gmail.com>
2026-06-29 10:54:47 +0530

    fix(backend): enable stats job

--- apps/backend/src/jobs/dailyStats.ts
+++ apps/backend/src/jobs/dailyStats.ts
@@ -1,16 +1,9 @@
 import { logger } from 'nb-logger';
-import { Network } from 'nb-types';
 
-import config from '#config';
 import sentry from '#libs/sentry';
 import { syncStats } from '#services/stats/dailyStats';
 
 export const task = async () => {
-  // TODO: testnet only for now
-  if (config.network === Network.MAINNET) {
-    return;
-  }
-
   try {
     logger.info('dailyStats: job started');
     await syncStats();
