1486fa82f6b7c9c39290bc8bf1753fe6f23776ea
Bayu <bayulaksana479@gmail.com>
2026-08-24 15:02:10 +0530

    fix(api): update account search api

--- apps/api/src/sql/queries/search/accounts.sql
+++ apps/api/src/sql/queries/search/accounts.sql
@@ -3,13 +3,13 @@ SELECT
 FROM
   accounts
 WHERE
-  account_id = ${account}
-  OR search @@ TO_TSQUERY(${account} || ':*')
+  account_id LIKE REPLACE(${account}, '_', '\_') || '%'
 ORDER BY
   (account_id = ${account}) DESC,
-  (account_id LIKE ${account} || '%') DESC,
-  (account_id LIKE ${account} || '.%') DESC,
+  (
+    account_id LIKE REPLACE(${account}, '_', '\_') || '.%'
+  ) DESC,
   LENGTH(account_id) ASC,
-  TS_RANK(search, TO_TSQUERY(${account} || ':*')) DESC
+  account_id ASC
 LIMIT
   5
