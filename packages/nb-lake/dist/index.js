974e0c3ffa627120e8f21a147c3ebbf1e9b5c601
Andrew Lau <andrew@andrewklau.com>
2026-08-24 12:50:02 +0700

    chore: run tests in CI and drop the orphaned nb-lake package
    turbo had no test task, so the suites in the repo never ran anywhere.
    Add one, wire it into CI, and fix nb-neardata's test script, which
    declared vitest with no test files: it exited 1 in CI and hung in watch
    mode locally.
    
    packages/nb-lake had no src, no dependents, and had not been touched
    since January.

--- packages/nb-lake/dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.types = exports.stream = exports.startStream = void 0;
-var streamer_1 = require("./streamer");
-Object.defineProperty(exports, "startStream", { enumerable: true, get: function () { return streamer_1.startStream; } });
-Object.defineProperty(exports, "stream", { enumerable: true, get: function () { return streamer_1.stream; } });
-exports.types = require("./types");
--- packages/nb-lake/dist/streamer.d.ts
+++
@@ -1,3 +0,0 @@
-import { LakeConfig, StreamerMessage } from "./types";
-export declare function stream(config: LakeConfig): AsyncIterableIterator<StreamerMessage>;
-export declare function startStream(config: LakeConfig, onStreamerMessageReceived: (data: StreamerMessage) => Promise<void>): Promise<void>;
