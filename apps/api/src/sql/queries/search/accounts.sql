e469d0ad67781a335539c4c49c9f057a9349d0e0
Bayu <bayulaksana479@gmail.com>
2026-09-18 10:06:36 +0530

    fix(api): update accounts search query

--- apps/api/src/sql/queries/search/accounts.sql
+++ apps/api/src/sql/queries/search/accounts.sql
@@ -1,18 +1,41 @@
+WITH
+  matched AS (
+    (
+      SELECT
+        account_id
+      FROM
+        accounts
+      WHERE
+        account_id LIKE REPLACE(${account}, '_', '\_') || '.%'
+      ORDER BY
+        account_id USING ~<~
+      LIMIT
+        20
+    )
+    UNION
+    (
+      SELECT
+        account_id
+      FROM
+        accounts
+      WHERE
+        account_id LIKE REPLACE(${account}, '_', '\_') || '%'
+      ORDER BY
+        account_id USING ~<~
+      LIMIT
+        100
+    )
+  )
 SELECT
   account_id
 FROM
-  accounts
-WHERE
-  account_id = ${account}
-  OR search @@ (QUOTE_LITERAL(${account}) || ':*')::TSQUERY
+  matched
 ORDER BY
   (account_id = ${account}) DESC,
-  (account_id LIKE ${account} || '%') DESC,
-  (account_id LIKE ${account} || '.%') DESC,
+  (
+    account_id LIKE REPLACE(${account}, '_', '\_') || '.%'
+  ) DESC,
   LENGTH(account_id) ASC,
-  TS_RANK(
-    search,
-    (QUOTE_LITERAL(${account}) || ':*')::TSQUERY
-  ) DESC
+  account_id ASC
 LIMIT
   5
